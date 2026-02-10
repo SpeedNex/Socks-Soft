e5e3b6c3b93f946f08db489c5601e1f59cb243e7
shenhaibing <haibing@ocerlink.com>
2026-02-10 09:07:27 +0800

    更新：配置

--- agent-install.sh
+++ agent-install.sh
@@ -6,7 +6,7 @@ AGENT_ID=""
 AGENT_SECRET=""
 REGION=""
 INSTALL_DIR="/usr/local/bin"
-BASE_URL="https://github.com/SpeedNex/Socks-Soft/raw/main/bin"
+BASE_URL="https://raw.githubusercontent.com/SpeedNex/Socks-Soft/main/proxy/bin"
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
